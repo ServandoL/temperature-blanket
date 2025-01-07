cddd32be0f332f5b12be559c111366dee6e43e1a
Servando <swyswy214@gmail.com>
2025-01-07 17:31:13 -0600

    fix css bug

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -49,7 +49,7 @@ export class AppComponent implements OnInit, OnDestroy {
 
   getClassForTemperature(temp: number): string {
     if (temp > 105) return 'color-105-plus';
-    if (temp < 30) return 'color-30-minus';
+    if (temp < 30) return 'color-29-minus';
     const lowerBound = Math.floor(temp / 5) * 5;
     const upperBound = lowerBound + 4;
     return `color-${lowerBound}-${upperBound}`;
