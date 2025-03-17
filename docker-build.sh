0b62f21171b0f93e1dab403d696c70def48bd197
Servando <swyswy214@gmail.com>
2025-03-17 09:18:22 -0500

    fix: Css changes

--- docker-build.sh
+++ docker-build.sh
@@ -1,3 +1,3 @@
 #!/bin/zsh
 docker build -t temperature-blanket-ui .
-docker run --name temperature-blanket-ui -d temperature-blanket-ui
+docker run -p 9000:8080 --name temperature-blanket-ui -d temperature-blanket-ui
